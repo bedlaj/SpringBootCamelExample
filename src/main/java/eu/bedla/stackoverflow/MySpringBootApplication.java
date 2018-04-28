ba2d564840c861357fa5cf652b2fa0e86ce14cae
Jan Bednář <mail@janbednar.eu>
2018-04-28 16:13:53 +0200

    Import another camel context with ImportResource

---
+++ src/main/java/eu/bedla/stackoverflow/MySpringBootApplication.java
@@ -0,0 +1,20 @@
+package eu.bedla.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ComponentScan({"eu.bedla.stackoverflow.routes"})//not required
+@ImportResource("classpath:META-INF/spring/camelContext.xml")
+public class MySpringBootApplication {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+
+}
